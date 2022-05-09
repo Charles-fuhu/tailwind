946b7edf1fe5004d4aff40313b429a9e6c83513e
Charles-fuhu <96430461+Charles-fuhu@users.noreply.github.com>
2022-05-09 12:57:23 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ tailwind-react/src/pages/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Header from '@/widgets/Header'
+import Hero from '@/widgets/Hero'
+import Solution from '@/widgets/Solution'
+import HowWork from '@/widgets/HowWork'
+import Clients from '@/widgets/Clients'
+import Contact from '@/widgets/Contact'
+function Index() {
+  return (
+    <div className='container mx-auto p-4'>
+      <Header />
+      {/* main------------ */}
+      <main className='mt-20'>
+        <Hero />
+      </main>
+      {/* section */}
+      <section className='md:p-20'>
+        <Solution></Solution>
+      </section>
+      {/* -------- */}
+      <section className='-mt-20'>
+        <HowWork></HowWork>
+      </section>
+      <section className='md:w-3/5 mx-auto mt-60'>
+        <Clients />
+      </section>
+      <section className='mt-20'>
+        <Contact></Contact>
+      </section>
+    </div>
+  )
+}
+
+export default Index
